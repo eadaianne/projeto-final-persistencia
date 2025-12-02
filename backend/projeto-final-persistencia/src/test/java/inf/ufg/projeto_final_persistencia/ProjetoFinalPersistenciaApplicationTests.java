a21883e0ebd5dad9800b9d867826e23fcfd7093e
eadaianne <eadaianne@gmail.com>
2025-12-02 12:09:06 -0300

    estrutura inicial backend

---
+++ backend/projeto-final-persistencia/src/test/java/inf/ufg/projeto_final_persistencia/ProjetoFinalPersistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package inf.ufg.projeto_final_persistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalPersistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
