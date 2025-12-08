9558841c1cc987f2c6267238ba06afe2a279bb51
eadaianne <eadaianne@gmail.com>
2025-12-08 15:21:28 -0300

    métodos base repositories

--- backend/projeto-final-persistencia/src/main/java/inf/ufg/projeto_final_persistencia/repositories/HospedagemRepository.java
+++ backend/projeto-final-persistencia/src/main/java/inf/ufg/projeto_final_persistencia/repositories/HospedagemRepository.java
@@ -3,5 +3,8 @@ package inf.ufg.projeto_final_persistencia.repositories;
 import inf.ufg.projeto_final_persistencia.entities.Hospedagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospedagemRepository extends JpaRepository<Hospedagem, Long> {
+    List<Hospedagem> findByPontoId(Long pontoId);
 }
